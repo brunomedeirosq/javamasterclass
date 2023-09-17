865ddb888c94ec34586c0f9be02aa53324cbd472
Bruno Queiros <brunoq@gmail.com>
2023-09-17 19:06:09 +0100

    java masterclass OOP Part 1 lessons 88 - 90

---
+++ Inheritance/src/Animal.java
@@ -0,0 +1,37 @@
+public class Animal {
+
+    //base class
+    //instance variables or class fields that are common to all animals
+    protected String type;
+    private String size;
+    private double weight;
+
+    public Animal() {
+
+    }
+
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    //instance methods or behaviors common to all animals
+    public void move(String speed) {
+        System.out.println(type + " moves " + speed);
+    }
+
+    public void makeNoise() {
+        System.out.println(type + " makes some kind of noise");
+    }
+
+}
