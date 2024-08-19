40fe5b5973831d4ddb4de39e38a339c614591bd1
Bruno Queiros <brunoq@gmail.com>
2024-08-19 12:01:51 +0100

    Fix to pass on coding exercise test on udemy compiler

--- CompositionExercise/src/Lamp.java
+++ CompositionExercise/src/Lamp.java
@@ -10,7 +10,7 @@ public class Lamp {
     }
 
     public void turnOn() {
-        System.out.println("Lamp -> Turning on");
+        System.out.print("Lamp -> Turning on");
     }
 
     public String getStyle() {
